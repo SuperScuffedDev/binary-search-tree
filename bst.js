50648619b286e48ec370ec53b17e3bee1324eb19
Michael Rodriguez <paininquires@gmail.com>
2026-03-09 23:53:18 -0400

    deduplication of input array insures or duplicate data makes it through the initial build of the tree

--- bst.js
+++ bst.js
@@ -1,6 +1,6 @@
 class Node {
     constructor(data, leftNode = null, rightNode = null) {
-        this.data = this.data;
+        this.data = data;
         this.leftNode = leftNode;
         this.rightNode = rightNode;
     }
@@ -8,20 +8,45 @@ class Node {
 
 class Tree {
     constructor(arr) {
-        this.root = this.#buildTree(arr, 0, arr.length - 1);
+        const dedupeArr = arr.filter((item, index) => {
+            return item !== arr[index - 1]
+        })
+        this.root = this.#buildTree(dedupeArr, 0, dedupeArr.length - 1);
     }
-
+    
     #buildTree(arr, start, end) {
+        if (start > end) return null;
+        
         const arrMid = start + Math.floor((end - start) / 2);
-        const root = new node(arr[arrMid]);
+        const root = new Node(arr[arrMid]);
 
-        root.leftNode = this.#buildTree(arr.slice(start, arrMid - 1), start, arrMid - 1);
-        root.rightNode = this.#buildTree(arr.slice(arrMid + 1, end), arrMid + 1, end);
+        root.leftNode = this.#buildTree(arr, start, arrMid - 1);
+        root.rightNode = this.#buildTree(arr, arrMid + 1, end);
 
         return root;
     }
 
+    includes(value) {}
 
+    insert(value) {}
+
+    deleteItem(value) {}
+
+    levelOrderForEach(callback) {}
+
+    preOrderForEach(callback) {}
+
+    inOrderForEach(callback) {}
+
+    postOrderForEach(callback) {}
+
+    height(value) {}
+
+    depth() {}
+
+    isBalanced() {}
+
+    rebalance() {}
 }
 
 module.exports = Tree;
